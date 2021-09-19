1e7720c89de4932ae30bb8581d0c1a94f2f51bd9
ptrLx <13c@posteo.de>
2021-09-19 21:43:49 +0200

    basic functionality

--- binary_counter_led_matrix/src/main.cpp
+++ binary_counter_led_matrix/src/main.cpp
@@ -1,9 +1,26 @@
-#include <Arduino.h>
+#include <MD_MAX72xx.h>
+
+#define HARDWARE_TYPE MD_MAX72XX::PAROLA_HW
+#define MAX_DEVICES 1
+#define CLK_PIN 13
+#define DATA_PIN 11
+#define CS_PIN 3
+
+MD_MAX72XX mx = MD_MAX72XX(HARDWARE_TYPE, CS_PIN, MAX_DEVICES);
+#define DELAYTIME 1000
+
+uint64_t i = 0;
 
 void setup() {
-  // put your setup code here, to run once:
+    mx.begin();
+    mx.clear();
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+    for (uint8_t col = 0; col < 8; col++) {
+        uint8_t bit_sequence = i >> col*8;
+        mx.setColumn(col, bit_sequence);
+    }
+    delay(DELAYTIME);
+    i++;
 }
